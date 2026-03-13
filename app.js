29865003e4e60e33f0cb77ee4dc6d6613352b7c9
Chintha Pavani <chinthapavani7@gmail.com>
2026-03-13 20:47:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+let student=[{name:'pavani',age:23,loc:'hyd'},{name:'chitti',age:22,loc:'nalgonda'},{name:'hari',age:23,loc:'kphb'},{name:'pallavi',age:25,loc:'ibp'},{name:'palli',age:24,loc:'panagal'}]
+console.log(student)
+console.log(student[2].loc)
+console.log(student[4])
+console.log(student[0].name)
+
+
